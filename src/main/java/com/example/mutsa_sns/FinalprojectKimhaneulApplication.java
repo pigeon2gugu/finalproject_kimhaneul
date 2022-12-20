ef7113d3dfec706d1c6d2510e8b544ef5da1cdb2
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-12-20 14:23:12 +0900

    first commit

---
+++ src/main/java/com/example/mutsa_sns/FinalprojectKimhaneulApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutsa_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectKimhaneulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectKimhaneulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mutsa_sns/FinalprojectKimhaneulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutsa_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectKimhaneulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
