5e56e5e25a76dbf4b30a02a9ea1e33c0e50acbd6
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-12-20 14:29:04 +0900

    hello controller 추가 - springSecurity 기본 설정

---
+++ src/main/java/com/example/mutsa_sns/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.mutsa_sns.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+//WebSecurityConfigurerAdapter 는 이후 SpringBoot에서 잘 안쓰게 됨
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/hello").permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
+                .and()
+                .build();
+    }
+
+}
