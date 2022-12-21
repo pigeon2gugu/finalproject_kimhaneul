8ad06d2fe4cbf0e80590679983e30de322fbe232
pigeon2gugu <khn1135@gmail.com>
2022-12-21 09:06:03 +0900

    hello controller endpoint 변경 - /hello -> /api/v1/hello

--- src/main/java/com/example/mutsa_sns/controller/HelloController.java
+++ src/main/java/com/example/mutsa_sns/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api/v1/")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("hello");
     }
