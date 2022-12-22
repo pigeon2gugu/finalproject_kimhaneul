964f17151ba1bc569f7b73183e8019c5ae9f59e9
pigeon2gugu <khn1135@gmail.com>
2022-12-23 00:05:44 +0900

    refactoring : PostControllerTest 수정 - @WithAnonymousUser test 수정

--- src/test/java/com/example/mutsa_sns/controller/PostControllerTest.java
+++ src/test/java/com/example/mutsa_sns/controller/PostControllerTest.java
@@ -14,10 +14,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,6 +31,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PostController.class)
@@ -60,16 +66,14 @@ class PostControllerTest {
     }
 
     @Test
-    @DisplayName("포스트 작성 실패 - 로그인 하지 않은 상태")
+    @DisplayName("포스트 작성 실패 - 로그인 하지 않은 상태(토큰 x)")
     @WithAnonymousUser
     void post_fail() throws Exception{
+
         PostCreateRequest postCreateRequest = PostCreateRequest.builder()
                 .title("title")
                 .body("body")
                 .build();
-
-        when(postService.createPost(any(), any()))
-                .thenThrow(new AppException(ErrorCode.INVALID_PERMISSION, ""));
 
         mockMvc.perform(post("/api/v1/posts")
                         .with(csrf())
@@ -79,4 +83,6 @@ class PostControllerTest {
                 .andExpect(status().isUnauthorized());
 
     }
+
+
 }
