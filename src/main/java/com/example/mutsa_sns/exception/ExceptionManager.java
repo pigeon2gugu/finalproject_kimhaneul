b0d141ddaf2dde2af872e2710066d1ba1fd0c028
pigeon2gugu <khn1135@gmail.com>
2022-12-22 14:40:54 +0900

    add : ExceptionHandler에 SQLException 추가

--- src/main/java/com/example/mutsa_sns/exception/ExceptionManager.java
+++ src/main/java/com/example/mutsa_sns/exception/ExceptionManager.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +20,13 @@ public class ExceptionManager {
 
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(new ErrorResponse(e.getErrorCode().name(), e.toString())));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlExceptionHandler(SQLException e){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.DATABASE_ERROR.name(), e.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(errorResponse));
     }
 
 }
