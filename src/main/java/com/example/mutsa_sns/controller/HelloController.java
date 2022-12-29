e354b89e2ad7c7d5debeacc30c88e2473cecb37c
pigeon2gugu <khn1135@gmail.com>
2022-12-29 09:48:16 +0900

    add : hello/{num} endpoint 추가 - 자리수 합을 출력하는 기능 추가

--- src/main/java/com/example/mutsa_sns/controller/HelloController.java
+++ src/main/java/com/example/mutsa_sns/controller/HelloController.java
@@ -1,15 +1,29 @@
 package com.example.mutsa_sns.controller;
+import com.example.mutsa_sns.service.HelloService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.criteria.CriteriaBuilder;
+
 @RestController
 @RequestMapping("/api/v1/")
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final HelloService helloService;
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("김하늘");
+    }
+
+    @GetMapping("/hello/{num}")
+    public String helloNum(int num) {
+        String addNum = helloService.add(num);
+
+        return addNum;
     }
 }
---
+++ src/main/java/com/example/mutsa_sns/service/HelloService.java
@@ -0,0 +1,22 @@
+package com.example.mutsa_sns.service;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HelloService {
+
+    public String add(int num) {
+
+        int addNum = 0;
+
+        while(num > 0) {
+            addNum += num%10;
+            num = num/10;
+        }
+
+        return String.valueOf(addNum);
+    }
+}
