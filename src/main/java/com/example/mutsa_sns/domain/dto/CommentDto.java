ad46096604e257c991fad12c2ef12969309d40bd
pigeon2gugu <khn1135@gmail.com>
2023-01-03 14:14:20 +0900

    feat : 댓글 달기 기능 추가 - Swagger : pageable기능 추가 - Post, Comment : soft delete로 삭제, deleteAt 추가 - ErrorCode : comment관련 errorCode 추가

---
+++ src/main/java/com/example/mutsa_sns/domain/dto/CommentDto.java
@@ -0,0 +1,22 @@
+package com.example.mutsa_sns.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class CommentDto {
+    private int id;
+    private String comment;
+    private String userName;
+    private int postId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+}
