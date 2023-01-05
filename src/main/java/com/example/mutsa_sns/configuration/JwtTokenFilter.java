ed42fea196d37e66910e4da083f7afb017e46c88
pigeon2gugu <khn1135@gmail.com>
2023-01-05 11:11:46 +0900

    refactor : JwtTokenFilter refactoring

--- src/main/java/com/example/mutsa_sns/configuration/JwtTokenFilter.java
+++ src/main/java/com/example/mutsa_sns/configuration/JwtTokenFilter.java
@@ -5,6 +5,9 @@ import com.example.mutsa_sns.exception.AppException;
 import com.example.mutsa_sns.exception.ErrorCode;
 import com.example.mutsa_sns.service.UserService;
 import com.example.mutsa_sns.utils.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -34,33 +37,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION); //헤더에 토큰을 넘긴 것을 가져옴.
 
-
-        //토큰이 없는 경우
-        if(authorizationHeader == null) {
-            request.setAttribute("exception", ErrorCode.INVALID_PERMISSION.name());
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        //bearer로 시작하는 토큰이 아닌 경우
-        if(!authorizationHeader.startsWith("Bearer ")) {
-            request.setAttribute("exception", ErrorCode.INVALID_TOKEN.name());
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         //bearer 이후 문자열 token 분리 성공 실패
         String token;
 
         try {
             token = authorizationHeader.split(" ")[1];
-
-            //만료된 토큰일 경우
-            if(JwtTokenUtil.isExpired(token, secretKey)) {
-                request.setAttribute("exception", ErrorCode.INVALID_TOKEN.name());
-                filterChain.doFilter(request, response);
-                return;
-            };
 
             // Token에서 UserName꺼내기 (JwtTokenUtil에서 Claim에서 꺼냄)
             String userName = JwtTokenUtil.getUserName(token, secretKey);
@@ -70,16 +51,20 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                     , List.of(new SimpleGrantedAuthority(user.getRole().name())));
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); //권한 부여
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            filterChain.doFilter(request,response);
-
-
-        } catch (Exception e) {
+        }
+        catch (SecurityException | MalformedJwtException | ExpiredJwtException e) {
+            request.setAttribute("exception", ErrorCode.INVALID_TOKEN.name());
+        }
+        catch (UnsupportedJwtException | IllegalArgumentException e) {
+            request.setAttribute("exception", ErrorCode.INVALID_PERMISSION.name());
+        }
+        catch (Exception e) {
             request.setAttribute("exception", ErrorCode.INVALID_TOKEN.name());
             filterChain.doFilter(request, response);
         }
-
-
-
+        finally {
+            filterChain.doFilter(request, response);
+        }
 
     }
 
